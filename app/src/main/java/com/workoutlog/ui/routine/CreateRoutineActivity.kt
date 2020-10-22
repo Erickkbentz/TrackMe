0eb8a596c9c2be41e35c062addb8fc4d05925215
Erick <ebenitezramos3@gatech.edu>
2020-10-22 15:19:58 -0400

    Routine name EditText autofocus on activity start

--- app/src/main/java/com/workoutlog/ui/routine/CreateRoutineActivity.kt
+++ app/src/main/java/com/workoutlog/ui/routine/CreateRoutineActivity.kt
@@ -1,19 +1,32 @@
 package com.workoutlog.ui.routine
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.workoutlog.R
+import kotlinx.android.synthetic.main.edit_list_view.*
 
 class CreateRoutineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_routine_view)
-        val routineNameEditText = findViewById<EditText>(R.id.routineNameEditText)
 
+        val routineNameEditText = findViewById<EditText>(R.id.routineNameEditText)
+        routineNameEditText.postDelayed({
+            routineNameEditText.requestFocus()
+            val inputMethodManage = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManage.showSoftInput(routineNameEditText, InputMethodManager.SHOW_IMPLICIT)
+        }, 100)
+
+        routineNameEditText.onDone{routineNameEditText.clearFocus()}
 
         val pplButton = findViewById<Button>(R.id.pplButton)
         pplButton.setOnClickListener {
@@ -29,6 +42,16 @@ class CreateRoutineActivity : AppCompatActivity() {
 
                 startActivity(intent)
             }
+        }
+    }
+
+    private fun EditText.onDone(callback: () -> Unit) {
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                callback.invoke()
+                true
+            }
+            false
         }
     }
 
